93b4995db34752a97c4e3b5e5bcceb12d6d4e165
Chris Whitworth <chris.whitworth@gmail.com>
2022-12-11 12:58:17 +0000

    format

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -2,19 +2,23 @@ use std::fs;
 mod monkey;
 
 fn parse_monkeys<'a, I>(lines: I) -> anyhow::Result<Vec<monkey::Monkey>>
-where I: Iterator<Item = &'a str> {
+where
+    I: Iterator<Item = &'a str>,
+{
     let mut monkeys = Vec::new();
     let mut iter = lines.peekable();
     loop {
         let line = iter.peek();
         if let Some(line_str) = line {
-        if (*line_str).trim().starts_with("Monkey") {
-            let test_lines = iter.by_ref().take(6).collect::<Vec<_>>();
-            monkeys.push(monkey::Monkey::parse(&test_lines)?);
+            if (*line_str).trim().starts_with("Monkey") {
+                let test_lines = iter.by_ref().take(6).collect::<Vec<_>>();
+                monkeys.push(monkey::Monkey::parse(&test_lines)?);
+            } else {
+                iter.next();
+            }
         } else {
-            iter.next();
-        } }
-        else { break; }
+            break;
+        }
     }
     Ok(monkeys)
 }
@@ -23,30 +27,36 @@ fn run_operation(op: &monkey::Operation, old: i64) -> i64 {
     match op {
         monkey::Operation::Add(x) => old + x,
         monkey::Operation::Mul(x) => old * x,
-        monkey::Operation::Sqr => old * old
+        monkey::Operation::Sqr => old * old,
     }
 }
 
 fn run_monkeys(monkeys: &mut Vec<monkey::Monkey>, how_worried: i64) -> anyhow::Result<Vec<usize>> {
     let mut inspected = Vec::new();
-    let all_divisible_by = monkeys.into_iter()
+    let all_divisible_by = monkeys
+        .into_iter()
         .map(|m| m.test.divisible_by)
-        .reduce(|acc, i| acc * i).unwrap();
+        .reduce(|acc, i| acc * i)
+        .unwrap();
 
     for monkey_index in 0..monkeys.len() {
         println!("Monkey {}", monkey_index);
         let mut monkey = &mut monkeys[monkey_index];
 
-        let v = monkey.items.clone().into_iter().map( |item| {
-            let mut worry_level = run_operation(&monkey.operation, item) / how_worried;
-            let throw_to = 
-            if worry_level % monkey.test.divisible_by == 0 {
-                monkey.test.pass_true
-            } else {
-                monkey.test.pass_false
-            };
-            (throw_to, worry_level % all_divisible_by)
-        }).collect::<Vec<_>>();
+        let v = monkey
+            .items
+            .clone()
+            .into_iter()
+            .map(|item| {
+                let mut worry_level = run_operation(&monkey.operation, item) / how_worried;
+                let throw_to = if worry_level % monkey.test.divisible_by == 0 {
+                    monkey.test.pass_true
+                } else {
+                    monkey.test.pass_false
+                };
+                (throw_to, worry_level % all_divisible_by)
+            })
+            .collect::<Vec<_>>();
 
         monkey.items.clear();
         inspected.push(v.len());
@@ -67,10 +77,14 @@ fn main() -> anyhow::Result<()> {
     let mut total = vec![0; monkeys.len()];
     for rounds in 0..20 {
         let passes = run_monkeys(&mut monkeys, 1)?;
-        total = passes.into_iter().enumerate().map(|(i,p)| total[i] + p).collect();
+        total = passes
+            .into_iter()
+            .enumerate()
+            .map(|(i, p)| total[i] + p)
+            .collect();
     }
     let mut monkey_business = total.clone();
-    monkey_business.sort_by(|a,b| b.cmp(a));
+    monkey_business.sort_by(|a, b| b.cmp(a));
     println!("{:?}", monkey_business[0] * monkey_business[1]);
 
     Ok(())
@@ -81,48 +95,52 @@ mod test {
     use crate::{monkey::*, parse_monkeys, run_monkeys};
 
     #[test]
-    fn test_parse_operation() -> anyhow::Result<()>
-    {
+    fn test_parse_operation() -> anyhow::Result<()> {
         for line in TEST_DATA.lines() {
             if line.starts_with("Operation:") {
-                println!("{:?}",Operation::parse(line)?);
+                println!("{:?}", Operation::parse(line)?);
             }
         }
         Ok(())
     }
 
     #[test]
-    fn test_parse_test() -> anyhow::Result<()>
-    {
+    fn test_parse_test() -> anyhow::Result<()> {
         let it = TEST_DATA.lines();
         let mut iter = it.peekable();
         loop {
             let line = iter.peek();
             if let Some(line_str) = line {
-            if line_str.trim().starts_with("Test") {
-                let test_lines = iter.by_ref().take(3).collect::<Vec<_>>();
-                println!("{:?}",Test::parse(&test_lines)?);
+                if line_str.trim().starts_with("Test") {
+                    let test_lines = iter.by_ref().take(3).collect::<Vec<_>>();
+                    println!("{:?}", Test::parse(&test_lines)?);
+                } else {
+                    iter.next();
+                }
             } else {
-                iter.next();
-            } }
-            else { break; }
+                break;
+            }
         }
 
         Ok(())
     }
 
     #[test]
-    fn test_parse_monkeys() -> anyhow::Result<()>{
+    fn test_parse_monkeys() -> anyhow::Result<()> {
         let it = TEST_DATA.lines();
         let mut monkeys = parse_monkeys(it)?;
 
         let mut total = vec![0; monkeys.len()];
         for rounds in 0..10000 {
             let passes = run_monkeys(&mut monkeys, 1)?;
-            total = passes.into_iter().enumerate().map(|(i,p)| total[i] + p).collect();
+            total = passes
+                .into_iter()
+                .enumerate()
+                .map(|(i, p)| total[i] + p)
+                .collect();
         }
         let mut monkey_business = total.clone();
-        monkey_business.sort_by(|a,b| b.cmp(a));
+        monkey_business.sort_by(|a, b| b.cmp(a));
 
         println!("{:?}, {}", total, monkey_business[0] * monkey_business[1]);
         Ok(())
@@ -133,8 +151,7 @@ mod test {
         assert!(true);
     }
 
-    const TEST_DATA: &str = 
-"Monkey 0:
+    const TEST_DATA: &str = "Monkey 0:
     Starting items: 79, 98
     Operation: new = old * 19
     Test: divisible by 23
--- day-11/src/monkey.rs
+++ day-11/src/monkey.rs
@@ -1,18 +1,17 @@
 use std::ops::Index;
 
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Operation {
     Add(i64),
     Mul(i64),
-    Sqr
+    Sqr,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Test {
     pub divisible_by: i64,
     pub pass_true: usize,
-    pub pass_false: usize
+    pub pass_false: usize,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -32,7 +31,10 @@ impl Operation {
 
             let oper_split = oper_str.split_whitespace().collect::<Vec<_>>();
             if oper_split[0] != "old" {
-                return Err(anyhow::anyhow!("Error parsing {} - function not in terms of old", line));
+                return Err(anyhow::anyhow!(
+                    "Error parsing {} - function not in terms of old",
+                    line
+                ));
             }
 
             match oper_split[1] {
@@ -42,32 +44,37 @@ impl Operation {
                     }
                     let add = oper_split[2].parse()?;
                     Ok(Operation::Add(add))
-                },
+                }
                 "*" => {
                     if oper_split[2] == "old" {
                         return Ok(Operation::Sqr);
                     }
                     let mul = oper_split[2].parse()?;
                     Ok(Operation::Mul(mul))
-                },
-                _ => {
-                    Err(anyhow::anyhow!("Error parsing {} - unknown operator", line))
                 }
+                _ => Err(anyhow::anyhow!("Error parsing {} - unknown operator", line)),
             }
         } else {
             Err(anyhow::anyhow!("Error parsing {}", line))
         }
-
     }
 }
 
 impl Test {
     pub fn parse(lines: &[&str]) -> anyhow::Result<Self> {
         if lines.len() != 3 {
-            return Err(anyhow::anyhow!("Error parsing {:?} - expected three lines, got {}", lines, lines.len()));
+            return Err(anyhow::anyhow!(
+                "Error parsing {:?} - expected three lines, got {}",
+                lines,
+                lines.len()
+            ));
         }
 
-        let mut t = Test { divisible_by: 0, pass_true: 0, pass_false: 0};
+        let mut t = Test {
+            divisible_by: 0,
+            pass_true: 0,
+            pass_false: 0,
+        };
 
         if let Some((_, divisible)) = lines[0].split_once(" by ") {
             t.divisible_by = divisible.parse()?;
@@ -78,8 +85,8 @@ impl Test {
         if let Some((cond, throw_to)) = lines[1].split_once(" monkey ") {
             if cond.contains("true:") {
                 t.pass_true = throw_to.parse()?;
-                } else {
-            return Err(anyhow::anyhow!("Error parsing {:?}", lines[1]));
+            } else {
+                return Err(anyhow::anyhow!("Error parsing {:?}", lines[1]));
             }
         } else {
             return Err(anyhow::anyhow!("Error parsing {:?}", lines[1]));
@@ -88,8 +95,8 @@ impl Test {
         if let Some((cond, throw_to)) = lines[2].split_once(" monkey ") {
             if cond.contains("false:") {
                 t.pass_false = throw_to.parse()?;
-                } else {
-            return Err(anyhow::anyhow!("Error parsing {:?}", lines[2]));
+            } else {
+                return Err(anyhow::anyhow!("Error parsing {:?}", lines[2]));
             }
         } else {
             return Err(anyhow::anyhow!("Error parsing {:?}", lines[2]));
@@ -106,8 +113,7 @@ impl Monkey {
                 return Err(anyhow::anyhow!("Error parsing {} - no monkey", lines[0]));
             }
             if let Some((_, id)) = lines[0].split_once(" ") {
-                if let Some(id_num) = id.strip_suffix(":")
-                {
+                if let Some(id_num) = id.strip_suffix(":") {
                     id_num.parse()?
                 } else {
                     return Err(anyhow::anyhow!("Error parsing {}", lines[0]));
@@ -122,7 +128,10 @@ impl Monkey {
                 return Err(anyhow::anyhow!("Error parsing {} - no items", lines[1]));
             }
             if let Some((_, items)) = lines[1].split_once(":") {
-                items.split(",").map(|i| i.trim().parse().unwrap()).collect::<Vec<_>>()
+                items
+                    .split(",")
+                    .map(|i| i.trim().parse().unwrap())
+                    .collect::<Vec<_>>()
             } else {
                 return Err(anyhow::anyhow!("Error parsing {}", lines[0]));
             }
@@ -132,6 +141,11 @@ impl Monkey {
 
         let test = Test::parse(&lines[3..6])?;
 
-        Ok(Monkey { id, items, operation, test })
+        Ok(Monkey {
+            id,
+            items,
+            operation,
+            test,
+        })
     }
 }
