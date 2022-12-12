cc1a79590c50c4b16de9657a089b35b85d0f6064
Chris Whitworth <chris.whitworth@microsoft.com>
2022-12-12 13:13:10 +0000

    Day 12 - input is broken

---
+++ day-12/src/main.rs
@@ -0,0 +1,168 @@
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fs, any,
+};
+
+mod vec2;
+use vec2::Vec2;
+
+#[derive(Clone, Debug)]
+enum NodeType {
+    Start,
+    End,
+    Step(usize),
+}
+
+struct Map {
+    nodes: Vec<NodeType>,
+    size: Vec2,
+    start: Vec2,
+    end: Vec2,
+}
+
+impl Map {
+    fn parse<'a, I>(lines: I) -> anyhow::Result<Map>
+    where
+        I: Iterator<Item = &'a str>,
+    {
+        let mut nodes = Vec::new();
+        let mut size = Vec2::O();
+        let mut start = Vec2::O();
+        let mut end = Vec2::O();
+        for line in lines {
+            size.x = line.len() as isize;
+            let chars = line.chars();
+            for (x, c) in chars.enumerate() {
+                let node_type = match c {
+                    'S' => {
+                        start = Vec2::from((x as isize, size.y));
+                        NodeType::Start
+                    }
+                    'E' => {
+                        end = Vec2::from((x as isize, size.y));
+                        NodeType::End
+                    }
+                    i => {
+                        if i.is_ascii_lowercase() {
+                            NodeType::Step(i as usize - 'a' as usize)
+                        } else {
+                            return Err(anyhow::anyhow!("Invalid node: {}", i));
+                        }
+                    }
+                };
+                nodes.push(node_type);
+            }
+            size.y += 1;
+        }
+        Ok(Map {
+            nodes,
+            size,
+            start,
+            end,
+        })
+    }
+
+    fn get(&self, p: Vec2) -> Option<NodeType> {
+        if p.x >= 0 && p.x < self.size.x && p.y >= 0 && p.y < self.size.y {
+            Some(self.nodes[(p.x + p.y * self.size.x) as usize].clone())
+        } else {
+            None
+        }
+    }
+}
+
+const SURROUNDING: [&str; 4] = ["R", "D", "U", "L"];
+
+fn solve(map: &Map) -> anyhow::Result<Vec<Vec2>> {
+    let mut visited: HashSet<Vec2> = HashSet::new();
+    let mut came_from: HashMap<Vec2, Vec2> = HashMap::new();
+    let mut to_visit: VecDeque<Vec2> = VecDeque::new();
+
+    to_visit.push_front(map.start.clone());
+
+    println!("Start: {:?}", map.start.clone());
+    println!("End: {:?}", map.end.clone());
+    loop {
+        if let Some(visiting) = to_visit.pop_front() {
+            let visiting_height = match map.get(visiting) {
+                Some(NodeType::Start) => 0,
+                Some(NodeType::End) => break,
+                Some(NodeType::Step(h)) => h,
+                None => { return Err(anyhow::anyhow!("Visiting non-existent node")); }
+            };
+            println!("Visiting: {:?} @ {}", visiting, visiting_height);
+            visited.insert(visiting.clone());
+            let mut will_visit = SURROUNDING
+                .iter()
+                .map(|dir| visiting.add(&Vec2::from(*dir)))
+                .filter(|p| {
+                    !visited.contains(p)
+                        && !to_visit.contains(p)
+                        && match map.get(*p) {
+                            Some(NodeType::Start) => false,
+                            Some(NodeType::End) => visiting_height == 24,
+                            Some(NodeType::Step(height)) => height.abs_diff(visiting_height) <= 1,
+                            None => false,
+                        }
+                })
+                .collect::<VecDeque<_>>();
+            println!("Will visit: {:?}", will_visit);
+
+            for p in will_visit.clone() {
+                came_from.insert(p, visiting);
+            }
+
+            to_visit.append(&mut will_visit);
+
+            //println!("Left to visit: {:?}", to_visit);
+        } else {
+            println!("{:?}", map.get(Vec2::from((160, 13))));
+            // println!("Visited: {:?}", visited);
+            return Err(anyhow::anyhow!("Didn't reach the end"));
+        }
+    }
+
+    let mut path: Vec<Vec2> = Vec::new();
+    let mut p = map.end;
+    loop {
+        path.push(p);
+        println!("{:?}",p);
+        if p == map.start {
+            break;
+        } else {
+            p = came_from[&p];
+        }
+    }
+
+    Ok(path)
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = fs::read_to_string("input")?;
+
+    let lines = input.lines();
+    let map = Map::parse(lines)?;
+
+    println!("{}", solve(&map)?.len());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const TEST_DATA: &str = "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
+
+    #[test]
+    fn test() -> anyhow::Result<()> {
+        let map = Map::parse(TEST_DATA.lines())?;
+
+        println!("{}",solve(&map)?.len());
+        Ok(())
+    }
+}
---
+++ day-12/src/vec2.rs
@@ -0,0 +1,67 @@
+use core::fmt;
+
+#[derive(Copy, Clone, Debug, PartialEq, Hash, Eq)]
+pub struct Vec2 {
+    pub x: isize,
+    pub y: isize,
+}
+
+impl From<&str> for Vec2 {
+    fn from(item: &str) -> Vec2 {
+        match item {
+            "U" => Vec2 { x: 0, y: 1 },
+            "R" => Vec2 { x: 1, y: 0 },
+            "D" => Vec2 { x: 0, y: -1 },
+            "L" => Vec2 { x: -1, y: 0 },
+            _ => panic!("Invalid direction"),
+        }
+    }
+}
+
+impl From<(isize, isize)> for Vec2 {
+    fn from(item: (isize, isize)) -> Vec2 {
+        Vec2 {
+            x: item.0,
+            y: item.1,
+        }
+    }
+}
+
+impl From<Vec2> for (isize, isize) {
+    fn from(v: Vec2) -> Self {
+        (v.x, v.y)
+    }
+}
+
+impl Vec2 {
+    pub fn new(x: isize, y: isize) -> Self {
+        Vec2 { x, y }
+    }
+
+    pub fn O() -> Self {
+        Vec2::from((0, 0))
+    }
+
+    pub fn direction(v1: &Vec2, v2: &Vec2) -> Vec2 {
+        Vec2::new((v1.x - v2.x).signum(), (v1.y - v2.y).signum())
+    }
+
+    pub fn add(&self, v: &Vec2) -> Vec2 {
+        Vec2::new(self.x + v.x, self.y + v.y)
+    }
+
+    pub fn sub(&self, v: &Vec2) -> Vec2 {
+        Vec2::new(self.x - v.x, self.y - v.y)
+    }
+
+    pub fn abs(&self) -> Vec2 {
+        Vec2::new(self.x.abs(), self.y.abs())
+    }
+}
+
+impl fmt::Display for Vec2 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!("({}, {})", self.x, self.y))?;
+        Ok(())
+    }
+}
